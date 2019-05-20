f9c284a5b0f22d3d14f8f380ccd522012dd48c97
ozanmert97 <50842337+ozanmert97@users.noreply.github.com>
2019-05-20 21:50:03 +0300

    Update hello_world.c
    Ozan Mert 040170744

--- Odev/hello_world.c
+++ Odev/hello_world.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(void)
-{
-  printf("Hello World !!!");
-  return 1;
+int main(void#include<conio.h>
+#include<math.h>
+#include <stdio.h>
+ 
+#define MAXCHAR 1000
+int main() {
+    FILE *fp;
+    char str[MAXCHAR];
+    char* filename = "c:\\Users\\ozanm\\Downloads\\veri.txt";
+ 
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+    while (fgets(str, MAXCHAR, fp) != NULL)
+        printf("%s", str);
+    fclose(fp);
+    return 0;
 }
