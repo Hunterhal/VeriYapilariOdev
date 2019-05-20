bf6e15e1145ecfe579d7ae7e86d95b03175dd01e
eneskoktas <50745051+eneskoktas@users.noreply.github.com>
2019-05-20 13:50:09 +0300

    Update hello_world.c
    Enes Köktaş / 040160052

--- Odev/hello_world.c
+++ Odev/hello_world.c
@@ -5,3 +5,11 @@ int main(void)
   printf("Hello World !!!");
   return 1;
 }
+
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("n"); 
+} 
